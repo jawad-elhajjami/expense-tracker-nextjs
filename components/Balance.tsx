54f944ccaa8e0bd0d80c49df2ade45aa8558b638
jawad-elhajjami <jawadelhajjamiofficiel@gmail.com>
2025-04-01 00:19:02 +0000

    fix some ui stuff

--- components/Balance.tsx
+++ components/Balance.tsx
@@ -4,8 +4,8 @@ import { addCommas } from "@/lib/utils";
 const Balance = async () => {
     const { balance } = await getUserBalance();
     return ( <>
-        <h4>Your balance</h4>
-        <h1>${addCommas(Number(balance?.toFixed(2) ?? 0))}</h1>
+        <h4 className="text-gray-500">Your balance</h4>
+        <h1 className="font-bold text-2xl">${addCommas(Number(balance?.toFixed(2) ?? 0))}</h1>
     </> );
 }
  
